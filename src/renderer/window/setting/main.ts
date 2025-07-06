6eeb3ccec061e820f6d158426cd33edc564b1d09
yajian.ke <keyajian@gmail.com>
2025-07-06 17:23:18 +0800

    fix: ui fix

--- src/renderer/window/setting/main.ts
+++ src/renderer/window/setting/main.ts
@@ -3,4 +3,10 @@ import App from './App.vue'
 import i18n from '../../i18n'
 import ElementPlus from 'element-plus'
 
+// 由于使用了按需引入，以下组件样式会丢失，暂时需要手动引入
+// import "element-plus/theme-chalk/el-message-box.css"
+// import "element-plus/theme-chalk/el-loading.css"
+// import "element-plus/theme-chalk/el-message.css"
+// import "element-plus/theme-chalk/el-notification.css"
+
 createApp(App).use(ElementPlus).use(i18n).mount('#app') 
